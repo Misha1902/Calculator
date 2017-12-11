27c338df67b60da09d2227ff37fcc54940c7ed4f
Misha1902 <misha.ib19@gmail.com>
2017-12-11 19:49:22 +0300

    something 3.2 (add anonum class)

--- app/src/main/java/com/example/student2/something/MainActivity.java
+++ app/src/main/java/com/example/student2/something/MainActivity.java
@@ -6,106 +6,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-int a;
-    TextView result;
-    EditText number1;
-    EditText number2;
-    Button plus;
-    Button minus;
-    Button multiplication;
-    Button division;
+
+    Spinner spinner;
+    private EditText editText;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        a = (int) (Math.random()*5+1);
-        final int startActivity = 1;
         super.onCreate(savedInstanceState);
-        switch (startActivity){
-            case (1): setContentView(R.layout.activity_main); break;
-            case (0):
-                switch (a) {
-                case 1: setContentView(R.layout.activity_main); break;
-                case 2: setContentView(R.layout.activity_main2); break;
-                case 3: setContentView(R.layout.activity_main3); break;
-                case 4: setContentView(R.layout.activity_main4); break;
-                case 5: setContentView(R.layout.activity_main5); break;
-            }
-            break;
-        }
-
-        result = (TextView) findViewById(R.id.NumOut);
-        number1 = (EditText) findViewById(R.id.Num1);
-        number2 = (EditText) findViewById(R.id.Num2);
-        plus = (Button) findViewById(R.id.plus);
-        minus = (Button) findViewById(R.id.minus);
-        multiplication = (Button) findViewById(R.id.multiplication);
-        division = (Button) findViewById(R.id.division);
-        final View.OnClickListener oclButton = new View.OnClickListener() {
+        setContentView(R.layout.activity2);
+        spinner =  (Spinner) findViewById(R.id.spinner);
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+        editText.setText("0");
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
-            public void onClick(View v) {
-
-                EditText Num1;
-                String Num1Text;
-                EditText Num2;
-                String Num2Text;
-                TextView NumOut;
-
-                switch (v.getId()) {
-                    case R.id.plus:
-                        Num1 = number1;
-                        Num1Text = Num1.getText().toString();
-                        Num2 = number2;
-                        Num2Text = Num2.getText().toString();
-                        NumOut = result;
-                        NumOut.setText("" + (Double.parseDouble(Num1Text) + Double.parseDouble(Num2Text)));
-                    break;
-                    case R.id.minus:
-                        Num1 = number1;
-                        Num1Text = Num1.getText().toString();
-                        Num2 = number2;
-                        Num2Text = Num2.getText().toString();
-                        NumOut = result;
-                        NumOut.setText("" + (Double.parseDouble(Num1Text) - Double.parseDouble(Num2Text)));
-                    break;
-                    case R.id.multiplication:
-                        Num1 = number1;
-                        Num1Text = Num1.getText().toString();
-                        Num2 = number2;
-                        Num2Text = Num2.getText().toString();
-                        NumOut = result;
-                        NumOut.setText("" + (Double.parseDouble(Num1Text) * Double.parseDouble(Num2Text)));
-                    break;
-                    case R.id.division:
-                        Num1 = number1;
-                        Num1Text = Num1.getText().toString();
-                        Num2 = number2;
-                        Num2Text = Num2.getText().toString();
-                        NumOut = result;
-                        NumOut.setText("" + (Double.parseDouble(Num1Text) / Double.parseDouble(Num2Text)));
-                    break;
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String choise = adapterView.getItemAtPosition(i).toString();
+                String num = editText.getText().toString();
+                String res = Integer.toBinaryString(Integer.parseInt(num));
+                try {
+                    switch (choise) {
+                        case "8":
+                            res = Integer.toOctalString(Integer.parseInt(num));
+                            break;
+                        case "16":
+                            res = Integer.toHexString(Integer.parseInt(num));
+                            break;
+                    }
+                    editText.setText(res);
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Только числа!", Toast.LENGTH_LONG).show();
                 }
             }
-        };
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-
-        plus.setOnClickListener(oclButton);
-        minus.setOnClickListener(oclButton);
-        multiplication.setOnClickListener(oclButton);
-        division.setOnClickListener(oclButton);
+            }
+        });
     }
-
-    public void MainActivity2(View view){
-        Intent intent = new  Intent (this, MainActivity2.class );
-        //Intent intent = new Intent(Intent.ACTION_VIEW);
-        //intent.setData(Uri.parse("http://myitschool.ru"));
-        intent.putExtra("number1Text", number1.getText().toString());
-        startActivity(intent);
-    }
-
 }
