7cfa314c3f9018704973562bd9d12cdc38c35490
Misha1902 <misha.ib19@gmail.com>
2018-01-26 20:18:36 +0300

    flow2.0

--- app/src/main/java/com/example/student2/something/MainActivity.java
+++ app/src/main/java/com/example/student2/something/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -25,24 +26,35 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-
 public class MainActivity extends AppCompatActivity {
+
+    boolean pressed = false;
     public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
         SurfaceHolder holder;
+        float x=0;
+        float y=0;
+        int r=150;
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event){
+            x = event.getX();
+            y = event.getY();
+            pressed = true;
+            return super.onTouchEvent(event);
+        }
+
         public MySurfaceView(Context context) {
             super(context);
             holder = getHolder();
             holder.addCallback(this);
-            p.setColor(Color.BLUE);
+            p.setColor(Color.YELLOW);
 
         }
         Paint p =new Paint();
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
-            Canvas canvas = holder.lockCanvas();
-            canvas.drawCircle(150, 150, 30, p);
-            holder.unlockCanvasAndPost(canvas);
+            new MyThread().start();
         }
 
         @Override
@@ -54,12 +66,44 @@ public class MainActivity extends AppCompatActivity {
         public void surfaceDestroyed(SurfaceHolder holder) {
 
         }
+
+
+
+        class MyThread extends Thread{
+
+            @Override
+            public void run(){
+                while(true){
+                    Canvas canvas = holder.lockCanvas();
+                    canvas.drawColor(Color.BLUE);
+                    if(pressed == true) {
+                        canvas.drawCircle(x, y, r, p);
+                    }
+                    holder.unlockCanvasAndPost(canvas);
+
+
+
+                       x++;
+                       y++;
+                       r += 5;
+                       try {
+                           Thread.sleep(1000);
+                       } catch (InterruptedException e) {
+                           e.printStackTrace();
+                       } finally {
+                       }
+
+                }
+            }
+        }
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MySurfaceView(this));
     }
+
 
 }
 
