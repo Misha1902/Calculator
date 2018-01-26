dedbc7c5d0fde316813e32d57c06a327e8891d19
Misha1902 <misha.ib19@gmail.com>
2018-01-26 18:49:59 +0300

    flow

--- app/src/main/java/com/example/student2/something/MainActivity.java
+++ app/src/main/java/com/example/student2/something/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.student2.something;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -17,49 +25,42 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+
 public class MainActivity extends AppCompatActivity {
+    public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+        SurfaceHolder holder;
+        public MySurfaceView(Context context) {
+            super(context);
+            holder = getHolder();
+            holder.addCallback(this);
+            p.setColor(Color.BLUE);
 
-    LinearLayout layout;
+        }
+        Paint p =new Paint();
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            Canvas canvas = holder.lockCanvas();
+            canvas.drawCircle(150, 150, 30, p);
+            holder.unlockCanvasAndPost(canvas);
+        }
 
-    Button general;
-    Button settingsButton;
-    Intent i;
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity2);
-        layout = (LinearLayout) findViewById(R.id.layout);
-        settingsButton = (Button) findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()){
-                    case R.id.settingsButton:
-                        i = new Intent(MainActivity.this, Settings.class);
-                        startActivity(i);
-                break;
-                }
-            }
-        });
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SharedPreferences preferences = getSharedPreferences("button", MODE_PRIVATE);
-        String side = preferences.getString("button_side", "");
-        Toast.makeText(this, side, Toast.LENGTH_SHORT).show();
-        switch(side){
-            case "TOP": layout.setGravity(Gravity.TOP);
-            break;
-            case "RIGHT": layout.setGravity(Gravity.RIGHT);
-            break;
-            case "LEFT": layout.setGravity(Gravity.LEFT);
-            break;
-            case "BOTTOM": layout.setGravity(Gravity.BOTTOM);
-            break;
-        }
+        setContentView(new MySurfaceView(this));
     }
 
 }
+
+
